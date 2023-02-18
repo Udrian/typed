a56cd8a7ca4f35a29c48118c94341beba585cd9c
Simon 'Udrian' Haggren <simonhaggren@gmail.com>
2023-02-18 20:51:03 +0100

    Fixed so that local module import product and dev dll path as well

--- TypeD/Models/Data/Module.cs
+++ TypeD/Models/Data/Module.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Reflection;
 
@@ -13,9 +14,9 @@ namespace TypeD.Models.Data
         public bool HaveDevModule { get { return !string.IsNullOrEmpty(Product.DevModuleName); } }
 
         // Paths
-        internal string ModulePath { get { return IsLocal ? Path.GetDirectoryName(Version.Replace(";local", "")) : Path.Combine(ModuleModel.ModuleCachePath, Name, Version); } }
-        internal string ModuleDLLPath { get { return IsLocal ? Version.Replace(";local", "") : Path.Combine(ModulePath, $"{Name}.dll"); } }
-        internal string ModuleDevDLLPath { get { return HaveDevModule ? (IsLocal ? Version.Replace(";local", "") : Path.Combine(ModulePath, $"{Product.DevModuleName}.dll")) : ""; } }
+        internal string ModulePath { get { return IsLocal ? Version.Replace(";local", "") : Path.Combine(ModuleModel.ModuleCachePath, Name, Version); } }
+        internal string ModuleDLLPath { get { return IsLocal ? Path.Combine(ModulePath, Name, "bin", "Debug", "net7.0", $"{Name}.dll") : Path.Combine(ModulePath, $"{Name}.dll"); } }
+        internal string ModuleDevDLLPath { get { return HaveDevModule ? (IsLocal ? Path.Combine(ModulePath, Product.DevModuleName, "bin", "Debug", "net7.0-windows", $"{Product.DevModuleName}.dll") : Path.Combine(ModulePath, $"{Product.DevModuleName}.dll")) : ""; } }
         internal string ProductPath { get { return Path.Combine(ModulePath, "product"); } }
 
         // Loaded data
