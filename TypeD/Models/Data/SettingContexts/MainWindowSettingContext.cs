d9d351e095d83429251bf9a294aa0db96fa4ef27
Udrian <simonhaggren@gmail.com>
2026-04-02 01:24:03 +0200

    Might as well, but should fix.

--- TypeD/Models/Data/SettingContexts/MainWindowSettingContext.cs
+++ TypeD/Models/Data/SettingContexts/MainWindowSettingContext.cs
@@ -29,7 +29,7 @@ namespace TypeD.Models.Data.SettingContexts
         public Setting<int> SizeY { get; set; } = new Setting<int>(768);
         public Setting<bool> Fullscreen { get; set; } = new Setting<bool>(true);
         public Setting<string> ExternalEditor { get; set; } = new Setting<string>("code {path}");
-        public Setting<string> ViewerType { get; set; } = new Setting<string>("TypeDCore.View.Viewer.ConsoleViewer");
+        public Setting<string> ViewerType { get; set; } = new Setting<string>("TypeDTK.View.Viewer.TKViewer"/*"TypeDCore.View.Viewer.ConsoleViewer"*/); //TODO: Make a better solution than this.
         public Setting<List<Panel>> Panels { get; set; }
 
         public MainWindowSettingContext()
